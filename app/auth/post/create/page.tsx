ea13e6597e8dc6be89b9c8d9472e1e148d66a54f
Willy_Awkward <willy1118t@gmail.com>
2024-03-25 22:23:49 +0800

    [refactor] Add PostTab component in CreatePage

--- app/auth/post/create/page.tsx
+++ app/auth/post/create/page.tsx
@@ -1,44 +1,20 @@
-"use client";
-import FormSection from "@/components/FormSection";
-import MarkdownRenderer from "@/components/MarkdownRenderer";
-import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
+import PostTab from "@/components/PostTab";
+import { auth } from "@/lib/auth";
 import Link from "next/link";
-import { useState } from "react";
-export default function CreatePage() {
-  const [title, setTitle] = useState<string>("");
-  const [body, setBody] = useState<string>("");
+import { redirect } from "next/navigation";
 
-  const handleTitleChange = (value: string) => {
-    setTitle(value);
-  };
+export default async function CreatePage() {
+  const session = await auth();
+  const admin = process.env.ADMIN_NAME;
 
-  const handleBodyChange = (value: string) => {
-    setBody(value);
-  };
+  if (!session || session.user.accountName !== admin) {
+    redirect("/");
+  }
 
   return (
     <>
       <Link href="/">Back</Link>
-      <Tabs defaultValue="form">
-        <TabsList className="grid w-full grid-cols-2 max-w-lg">
-          <TabsTrigger value="form">Form</TabsTrigger>
-          <TabsTrigger value="preview">Preview</TabsTrigger>
-        </TabsList>
-        <TabsContent value="form">
-          <FormSection
-            setTitle={handleTitleChange}
-            setBody={handleBodyChange}
-            title={title}
-            body={body}
-          />
-        </TabsContent>
-        <TabsContent value="preview">
-          <div className="max-w-lg p-4 bg-white rounded shadow-md">
-            <h1 className="text-xl font-bold border-b mb-4">{title}</h1>
-            <MarkdownRenderer body={body} />
-          </div>
-        </TabsContent>
-      </Tabs>
+      <PostTab type="create" />
     </>
   );
 }
---
+++ components/PostTab.tsx
@@ -0,0 +1,46 @@
+"use client";
+
+import { useState } from "react";
+import FormSection from "@/components/FormSection";
+import MarkdownRenderer from "@/components/MarkdownRenderer";
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
+
+export default function PostTab({
+  initTitle,
+  initBody,
+  issueNum,
+  type,
+}: {
+  initTitle?: string;
+  initBody?: string;
+  issueNum?: number;
+  type: "create" | "edit";
+}) {
+  const [title, setTitle] = useState<string>(initTitle ?? "");
+  const [body, setBody] = useState<string>(initBody ?? "");
+
+  return (
+    <Tabs defaultValue="form">
+      <TabsList className="grid w-full grid-cols-2 max-w-lg">
+        <TabsTrigger value="form">Form</TabsTrigger>
+        <TabsTrigger value="preview">Preview</TabsTrigger>
+      </TabsList>
+      <TabsContent value="form">
+        <FormSection
+          setTitle={setTitle}
+          setBody={setBody}
+          title={title}
+          body={body}
+          issueNum={issueNum}
+          type={type}
+        />
+      </TabsContent>
+      <TabsContent value="preview">
+        <div className="max-w-lg p-4 bg-white rounded shadow-md">
+          <h1 className="text-xl font-bold border-b mb-4">{title}</h1>
+          <MarkdownRenderer body={body} />
+        </div>
+      </TabsContent>
+    </Tabs>
+  );
+}
