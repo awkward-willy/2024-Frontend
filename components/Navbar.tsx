21beb3de967427b4a4ac0dc4665fd27e14998ea7
Willy_Awkward <willy1118t@gmail.com>
2024-03-28 14:23:15 +0800

    [feat] Add dropdown to Navbar for logout

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -3,29 +3,46 @@ import Link from "next/link";
 
 import { Button } from "@/components/ui/button";
 import { auth } from "@/lib/auth";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+} from "@components/ui/dropdown-menu";
+import { PlusIcon } from "@radix-ui/react-icons";
 
 export default async function Navbar() {
   const admin = process.env.ADMIN_NAME;
   const session = await auth();
   if (session) {
     return (
-      <nav className="sticky top-0 flex items-center border-b-2 backdrop-blur-sm">
-        <h1 className="my-4 flex-grow">Dcard 前端實習作業</h1>
-        {session.user.accountName === admin && (
-          <Button variant="link" asChild>
-            <Link href="/auth/post/create">Create</Link>
-          </Button>
-        )}
-        <Button variant="link" asChild>
-          <Link href="/api/auth/signout">Signout</Link>
-        </Button>
-        <Image
-          src={session.user.image ?? ""}
-          alt="user avatar"
-          width={40}
-          height={40}
-          className="rounded-full"
-        />
+      <nav className="sticky top-0 z-10 flex items-center justify-between bg-accent px-4 py-2">
+        <h1 className="text-lg font-bold md:text-xl">丹尼爾的部落格</h1>
+        <div className="flex">
+          {session.user.accountName === admin && (
+            <Button variant="ghost" asChild>
+              <Link href="/auth/post/create">
+                <PlusIcon className="h-6 w-6" />
+              </Link>
+            </Button>
+          )}
+          <DropdownMenu>
+            <DropdownMenuTrigger>
+              <Image
+                src={session.user.image ?? ""}
+                alt="user avatar"
+                width={40}
+                height={40}
+                className="min-h-4 min-w-4 rounded-full"
+              />
+            </DropdownMenuTrigger>
+            <DropdownMenuContent align="start">
+              <DropdownMenuItem>
+                <Link href="/api/auth/signout">登出</Link>
+              </DropdownMenuItem>
+            </DropdownMenuContent>
+          </DropdownMenu>
+        </div>
       </nav>
     );
   }
