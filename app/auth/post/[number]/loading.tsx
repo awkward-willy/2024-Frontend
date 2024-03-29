25be84878b06b69ee7a40c6825553709f69dc550
Willy_Awkward <willy1118t@gmail.com>
2024-03-29 10:24:38 +0800

    [style] Style LoadingPost page

--- app/auth/post/[number]/loading.tsx
+++ app/auth/post/[number]/loading.tsx
@@ -1,3 +1,10 @@
-export default function loading() {
-  return <div>loading</div>;
+export default function LoadingPost() {
+  return (
+    <div className="flex items-baseline gap-2">
+      <span className="font-bold">讀取中</span>
+      <div className="h-1 w-1 animate-bounce rounded-full bg-white [animation-delay:-0.3s]" />
+      <div className="h-1 w-1 animate-bounce rounded-full bg-white [animation-delay:-0.15s]" />
+      <div className="h-1 w-1 animate-bounce rounded-full bg-white" />
+    </div>
+  );
 }
