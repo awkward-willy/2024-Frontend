28ef7f011569ffc04b98515c674c2064aaa74eea
Willy_Awkward <willy1118t@gmail.com>
2024-03-30 23:05:09 +0800

    [feat] Add Delete confirmation for post & comment

--- components/Comment/DeleteCommentButton.tsx
+++ components/Comment/DeleteCommentButton.tsx
@@ -5,10 +5,16 @@ import { toast } from "sonner";
 import { deleteComment } from "@/actions/deleteComment";
 import { Button } from "@components/ui/button";
 import {
+  Popover,
+  PopoverContent,
+  PopoverTrigger,
+} from "@components/ui/popover";
+import {
   CheckCircledIcon,
   CrossCircledIcon,
   TrashIcon,
 } from "@radix-ui/react-icons";
+import { PopoverClose } from "@radix-ui/react-popover";
 
 interface DeleteCommentButtonProps {
   id: string;
@@ -22,26 +28,39 @@ export default function DeleteCommentButton({
   removeComment,
 }: DeleteCommentButtonProps) {
   return (
-    <Button
-      variant="link"
-      className="hover:text-red-400"
-      onClick={async () => {
-        await deleteComment({ id, token }).then((status) => {
-          if (status === 204) {
-            toast("成功刪除評論", {
-              icon: <CheckCircledIcon color="green" />,
+    <Popover>
+      <PopoverTrigger asChild>
+        <Button variant="link">
+          <TrashIcon height="20" width="20" className="hover:text-red-400" />
+        </Button>
+      </PopoverTrigger>
+      <PopoverContent className="flex w-fit flex-col items-center justify-center gap-2 bg-white">
+        <p className="text-black">確定要刪除嗎？</p>
+        <Button
+          variant="link"
+          className="text-red-400"
+          onClick={async () => {
+            await deleteComment({ id, token }).then((status) => {
+              if (status === 204) {
+                toast("成功刪除評論", {
+                  icon: <CheckCircledIcon color="green" />,
+                });
+                removeComment(id);
+              } else {
+                toast("刪除評論失敗", {
+                  description: "錯誤碼：" + status,
+                  icon: <CrossCircledIcon color="red" />,
+                });
+              }
             });
-            removeComment(id);
-          } else {
-            toast("刪除評論失敗", {
-              description: "錯誤碼：" + status,
-              icon: <CrossCircledIcon color="red" />,
-            });
-          }
-        });
-      }}
-    >
-      <TrashIcon height="20" width="20" />
-    </Button>
+          }}
+        >
+          確定
+        </Button>
+        <PopoverClose className="text-black" asChild>
+          <Button variant="link">取消</Button>
+        </PopoverClose>
+      </PopoverContent>
+    </Popover>
   );
 }
--- components/Post/DeletePostButton.tsx
+++ components/Post/DeletePostButton.tsx
@@ -1,36 +1,52 @@
 "use client";
 
-import { useRouter } from "next/navigation";
 import { toast } from "sonner";
 
 import { deletePost } from "@/actions/deletePost";
 import { Button } from "@components/ui/button";
+import {
+  Popover,
+  PopoverContent,
+  PopoverTrigger,
+} from "@components/ui/popover";
 import { TrashIcon } from "@radix-ui/react-icons";
+import { PopoverClose } from "@radix-ui/react-popover";
 
 export default function DeletePostButton({
   postNumber,
 }: {
   postNumber: string;
 }) {
-  const router = useRouter();
   return (
-    <Button
-      variant="link"
-      className="hover:text-red-400"
-      onClick={async () => {
-        await deletePost(postNumber).then((res) => {
-          if (res === 200) {
-            toast("成功刪除文章");
-          } else {
-            toast("刪除文章失敗", {
-              description: "Error code: " + res,
+    <Popover>
+      <PopoverTrigger asChild>
+        <Button variant="link">
+          <TrashIcon height="20" width="20" className="hover:text-red-400" />
+        </Button>
+      </PopoverTrigger>
+      <PopoverContent className="flex w-fit flex-col items-center justify-center gap-2 bg-white">
+        <p className="text-black">確定要刪除嗎？</p>
+        <Button
+          variant="link"
+          className="text-red-400"
+          onClick={async () => {
+            await deletePost(postNumber).then((res) => {
+              if (res === 200) {
+                toast("成功刪除文章");
+              } else {
+                toast("刪除文章失敗", {
+                  description: "Error code: " + res,
+                });
+              }
             });
-          }
-          router.refresh();
-        });
-      }}
-    >
-      <TrashIcon height="20" width="20" />
-    </Button>
+          }}
+        >
+          確定
+        </Button>
+        <PopoverClose className="text-black">
+          <Button variant="link">取消</Button>
+        </PopoverClose>
+      </PopoverContent>
+    </Popover>
   );
 }
