7810374875c008d34530a70ddb03e44ccdcfe960
Willy_Awkward <willy1118t@gmail.com>
2024-03-30 22:41:00 +0800

    [feat] Access control for post editing

--- app/auth/post/[number]/edit/page.tsx
+++ app/auth/post/[number]/edit/page.tsx
@@ -1,7 +1,9 @@
 import { Metadata } from "next";
+import { notFound, redirect } from "next/navigation";
 
 import { fetchPostByNumber } from "@/actions/fetchPostByNumber";
 import { auth } from "@/lib/auth";
+import env from "@/lib/env";
 import { Post } from "@/types/Post";
 import BackToAuthButton from "@components/BackToAuthButton";
 import PostTab from "@components/Post/PostTab";
@@ -15,11 +17,21 @@ export default async function EditPostNumberPage({
 }: {
   params: { number: string };
 }) {
+  const adminName = env.GITHUB_ADMIN_NAME;
   const session = await auth();
   const postData: Post = await fetchPostByNumber({
     number: params.number,
     token: session?.access_token,
   });
+
+  if (!session) {
+    redirect("/");
+  }
+
+  if (postData.state === "closed" || session.user.accountName !== adminName) {
+    notFound();
+  }
+
   if (session) {
     return (
       <>
