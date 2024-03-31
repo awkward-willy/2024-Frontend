e7c0160ff655420251d942cc4fe47b5943895b3a
Willy_Awkward <willy1118t@gmail.com>
2024-03-31 17:20:09 +0800

    [fix] Make button in not-found asChild

--- app/auth/post/not-found.tsx
+++ app/auth/post/not-found.tsx
@@ -6,7 +6,7 @@ export default function NotFound() {
   return (
     <div className="flex flex-col gap-2">
       <h1>很抱歉，無法找到該貼文。</h1>
-      <Button variant="outline" className="max-w-40">
+      <Button variant="outline" className="max-w-40" asChild>
         <Link href="/">返回貼文列</Link>
       </Button>
     </div>
